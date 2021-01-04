37d1f3c19b024fa161baf7758f00c79117d1ba97
YRM <yinruiming1219@163.com>
2021-01-04 18:57:12 +0800

    1、Math.random()

--- src/main/java/com/xy/algorithm/xiaobai/RandToRand.java
+++ src/main/java/com/xy/algorithm/xiaobai/RandToRand.java
@@ -4,8 +4,24 @@ public class RandToRand {
 
     public static void main(String[] args) {
         System.out.println("測試開始");
-        // Math.random() -> double -> [0,1)
+        // Math.random() -> double -> [0,1)      0,1 均匀分布伪随机返回
         double ans = Math.random();
+
+
+        System.out.println(ans);
+
+        System.out.println("--------------");
+
+        int testTimes = 100000000;
+        int count = 0;
+        for (int i = 0; i < testTimes; i++){
+            if (Math.random() < 0.75){
+                count++;
+            }
+        }
+        System.out.println((double) count /(double) testTimes);
+        System.out.println((double) count);
+
     }
 
 }
